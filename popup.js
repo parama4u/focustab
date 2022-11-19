7e5fd357edb188a8dd6d5e0bf5fc605c7124c897
Param <parama4u@gmail.com>
2022-11-19 18:39:25 +0900

    working copy:list all open tabs

--- popup.js
+++ popup.js
@@ -1,3 +1,26 @@
-addEventListener('DOMContentLoaded', function() {
+// const tabs = await chrome.tabs.query({
+//     url: [
+//       "https://developer.chrome.com/docs/webstore/*",
+//       "https://developer.chrome.com/docs/extensions/*",
+//     ],
+//   });
 
-})
+let queryOptions = { };
+const tabs = await chrome.tabs.query(queryOptions);
+
+
+const template = document.getElementById("opentab");
+const elements = new Set();
+
+
+  for (const tab of tabs) {
+    const element = template.content.firstElementChild.cloneNode(true);
+  
+    const title = tab.title.split("-")[0].trim();
+    // const pathname = new URL(tab.url).pathname.slice("/docs".length);
+    const pathname = tab.url
+    element.querySelector(".title").textContent = title;
+    element.querySelector(".pathname").textContent = pathname;
+    elements.add(element);
+  }
+  document.querySelector("ul").append(...elements);
