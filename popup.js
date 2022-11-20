9693e4de997d3ea698d41812ba03d65d66903436
Param <parama4u@gmail.com>
2022-11-20 13:11:47 +0900

    adding data to local

--- popup.js
+++ popup.js
@@ -1,26 +1,33 @@
-// const tabs = await chrome.tabs.query({
-//     url: [
-//       "https://developer.chrome.com/docs/webstore/*",
-//       "https://developer.chrome.com/docs/extensions/*",
-//     ],
-//   });
+
+// Bulding the list
 
 let queryOptions = { };
 const tabs = await chrome.tabs.query(queryOptions);
-
-
 const template = document.getElementById("opentab");
 const elements = new Set();
 
-
   for (const tab of tabs) {
     const element = template.content.firstElementChild.cloneNode(true);
-  
     const title = tab.title.split("-")[0].trim();
     // const pathname = new URL(tab.url).pathname.slice("/docs".length);
     const pathname = tab.url
     element.querySelector(".title").textContent = title;
-    element.querySelector(".pathname").textContent = pathname;
+    // element.querySelector(".pathname").textContent = pathname;
     elements.add(element);
+    setConfig()
   }
   document.querySelector("ul").append(...elements);
+
+  function getConfig(){
+
+  }
+
+  function setConfig(title_pattern = null, url_pattern = null, stale_time = 60){
+    let data = {
+      title_pattern: "Google Search",
+      url_pattern : "google.com",
+      stale_time : 60
+    };
+   chrome.storage.local.set(data);
+
+  }
