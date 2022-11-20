5c4a6f947266ca891af9e7612917dab34334451b
Param <parama4u@gmail.com>
2022-11-20 20:52:19 +0900

    updates

--- popup.js
+++ popup.js
@@ -8,18 +8,26 @@ const elements = new Set();
 
   for (const tab of tabs) {
     const element = template.content.firstElementChild.cloneNode(true);
-    const title = tab.title.split("-")[0].trim();
+    const title = tab.title;
     // const pathname = new URL(tab.url).pathname.slice("/docs".length);
     const pathname = tab.url
     element.querySelector(".title").textContent = title;
     // element.querySelector(".pathname").textContent = pathname;
     elements.add(element);
-    setConfig()
+    setConfig();
+    getConfig(tab);
   }
   document.querySelector("ul").append(...elements);
 
-  function getConfig(){
-
+  function getConfig(tab){
+    let data = chrome.storage.local.get();
+    if(tab.title.match(data["title_pattern"]))
+      {
+        console.log("its a mattch" + tab.title);
+    }
+    else{
+      console.log("Not a match");
+    }
   }
 
   function setConfig(title_pattern = null, url_pattern = null, stale_time = 60){
