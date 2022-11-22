0149e6caea471a2203ceccc5712d45c357efd5b5
Param <parama4u@gmail.com>
2022-11-23 02:03:02 +0900

    rewriting the css and js

--- popup.js
+++ popup.js
@@ -6,33 +6,77 @@ const tabs = await chrome.tabs.query(queryOptions);
 const template = document.getElementById("opentab");
 const elements = new Set();
 
-  for (const tab of tabs) {
+  // for (const tab of tabs) {
+  //   const element = template.content.firstElementChild.cloneNode(true);
+  //   const title = tab.title;
+  //   const pathname = tab.url;
+  //   element.querySelector(".title").textContent = title;
+  //   elements.add(element);
+  //   setConfig(tab.id.toString(),tab.title,tab.url);
+  //   let test = Object.getOwnPropertyNames(getConfig(tab));
+  //   console.log("result:" + test);
+  // }
+  // document.querySelector("ul").append(...elements);
+
+  async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
     const element = template.content.firstElementChild.cloneNode(true);
-    const title = tab.title;
-    // const pathname = new URL(tab.url).pathname.slice("/docs".length);
-    const pathname = tab.url;
-    element.querySelector(".title").textContent = title;
-    // element.querySelector(".pathname").textContent = pathname;
-    elements.add(element);
-    setConfig();
-    getConfig(tab);
+    element.querySelector(".title").textContent = tab.title;
+    document.querySelector(".items").append(element);
+    return tab;
   }
-  document.querySelector("ul").append(...elements);
+
+  async function getOpenTabs(){
+    let queryOptions = { active: false };
+    const tabs = await chrome.tabs.query(queryOptions);
+    for (const tab of tabs) {
+      const element = template.content.firstElementChild.cloneNode(true);
+      element.querySelector(".title").textContent = tab.title;
+      elements.add(element);
+    }
+    document.querySelector(".items").append(...elements);
+  }
+
+  getCurrentTab();
+  getOpenTabs();
 
   function getConfig(tab){
-                const res = chrome.storage.local.get("title_pattern",function(title_pattern){
-                          return tab.title.search(title_pattern.title_pattern);
-                        });
-                console.log (tab.title + ":" + res );
-      }  
+              let res = {}
+              console.log(tab.id);
+              let tab_id=tab.id.toString();
+              chrome.storage.local.get(tab_id,function(data){ 
+                        Object.assign(res,data);
+                        console.log("Result0:"+ data[tab_id].stale_config);  
+                        }
+                        );
+                      console.log("Result1:"+ data[tab_id]);
+                      return res;
+                      }
+  
+  function closeTab(tab_id)
+        {
+          //call this function when stale_time >= config time
+          return;
+        }
         
 
-  function setConfig(title_pattern = null, url_pattern = null, stale_time = 60){ 
-    let data = {
-      title_pattern: "Google Search",
-      url_pattern : "google.com",
-      stale_time : 60
+  function setConfig(tab_id=null,title_pattern=null, url_pattern = null, stale_time = 60){ 
+    let config = {
+      title_pattern: title_pattern,
+      url_pattern: url_pattern,
+      stale_config: stale_time,
+      stale_current:0,
     };
-   chrome.storage.local.set(data);
+    let data = {};
+    data[tab_id] = config;
+    console.log("Setting config" + data[tab_id].stale_config + " for:" + tab_id);
+    chrome.storage.local.set(data);
 
+  }
+
+  function removeConfig(tab_id)
+  {
+  return;
   }
